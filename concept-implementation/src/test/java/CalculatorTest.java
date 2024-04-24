f53cded8573e015f919e7586e1019a99a1ccc29f
nitichaikardkarnklai <152477400+nitichaikardkarnklai@users.noreply.github.com>
2024-04-24 12:41:14 +0700

    @DisplayName

--- concept-implementation/src/test/java/CalculatorTest.java
+++ concept-implementation/src/test/java/CalculatorTest.java
@@ -1,3 +1,4 @@
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -5,6 +6,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class CalculatorTest {
 
     @Test
+    @DisplayName("1 + 1 = 2")
     void addShouldReturn2() {
         // Arrange
         Calculator calculator = new Calculator();
@@ -17,6 +19,7 @@ public class CalculatorTest {
     }
 
     @Test
+    @DisplayName("input 1 and 2 output should be 3")
     void addShouldReturn3() {
         Calculator calculator = new Calculator();
         int actual = calculator.add(1,2);
